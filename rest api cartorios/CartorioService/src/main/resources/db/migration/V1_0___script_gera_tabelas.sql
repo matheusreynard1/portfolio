959cf76d01d9ce462e1003c45bd7bcd30b341ad4
matheusreynard1 <117281662+matheusreynard1@users.noreply.github.com>
2024-01-05 10:39:04 -0300

    Delete rest api cartorios directory

--- rest api cartorios/CartorioService/src/main/resources/db/migration/V1_0___script_gera_tabelas.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Cadastro_Cartorio
-(nome, observacao, situacao_cartorio, lista_atribuicoes)
-VALUES('Nome teste 3', 'observacao 3', 1, 'lista 3');
-INSERT INTO Cadastro_Cartorio
-(nome, observacao, situacao_cartorio, lista_atribuicoes)
-VALUES('Nome teste 2', 'observacao 2', 1, 'lista 2');
